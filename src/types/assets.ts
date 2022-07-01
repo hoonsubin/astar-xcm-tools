c8e5bf9dea72f25db485476e4b4cea2b7502ea57
Hoon Kim <mail@hoonkim.me>
2022-07-01 18:43:26 +0200

    Update assets.ts

--- src/types/assets.ts
+++ src/types/assets.ts
@@ -1,5 +1,6 @@
 import { AssetMetadata, AssetDetails } from '@polkadot/types/interfaces';
 
+// todo: refactor this so is's a generic asset representation
 export interface ChainAsset extends AssetDetails {
     id: string;
     metadata: AssetMetadata
